7338f2170bb4ef3b0f7672836bf75d076c168bfe
ferreiraluiza <annaluizaferreira1306@gmail.com>
2024-10-11 23:32:18 -0300

    parei por aqui hj

---
+++ frontend/src/pages/landing/index.jsx
@@ -0,0 +1,13 @@
+import './index.scss'
+import { useState } from 'react';
+import './App.scss';
+import axios from "axios";
+
+export default function app(){
+    alert('ta funcionando :/')
+}
+
+return(
+     
+    
+)
